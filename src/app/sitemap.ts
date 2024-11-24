6b3bf561e620126c947da53272518f40ae426db1
OreQr <50458706+OreQr@users.noreply.github.com>
2024-11-24 22:48:43 +0100

    Fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return hooksConfig.map((hook) => ({
     url: new URL(
       "/hook/" + hook.name,
-      process.env.VERCEL_URL ?? "http://localhost:3000"
+      process.env.VERCEL_URL
+        ? "https://" + process.env.VERCEL_URL
+        : "http://localhost:3000"
     ).href,
   }))
 }
