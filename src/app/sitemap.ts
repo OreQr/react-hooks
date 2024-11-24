412ba2cc3379832a36ee8011187f9fbc028639a5
OreQr <50458706+OreQr@users.noreply.github.com>
2024-11-24 23:14:38 +0100

    Change sitemap url to production url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,8 +6,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return hooksConfig.map((hook) => ({
     url: new URL(
       "/hook/" + hook.name,
-      process.env.VERCEL_URL
-        ? "https://" + process.env.VERCEL_URL
+      process.env.VERCEL_PROJECT_PRODUCTION_URL
+        ? "https://" + process.env.VERCEL_PROJECT_PRODUCTION_URL
         : "http://localhost:3000"
     ).href,
   }))
