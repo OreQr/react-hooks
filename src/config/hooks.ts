167e79ff811e5ee2a726b880c0b551791626bb4d
OreQr <50458706+OreQr@users.noreply.github.com>
2024-11-25 01:10:38 +0100

    Ref usePrevious hook config

--- src/config/hooks.ts
+++ src/config/hooks.ts
@@ -4,21 +4,19 @@ export const hooksConfig: Hook[] = [
   {
     name: "usePrevious",
     fileName: "use-previous",
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industrys standard dummy text
-            ever since the 1500s.`,
+    description: `Returns the previous value of a given input. Useful for comparing previous and current values in a component lifecycle.`,
     parameters: [
       {
         name: "value",
         type: "any",
-        description: "input value",
+        description: "Value to track the previous state of.",
       },
     ],
     returnValue: [
       {
-        name: "previous value",
+        name: "previousValue",
         type: "value type | undefined",
-        description: "previous value based on input value",
+        description: "Previous value of the provided input.",
       },
     ],
   },
